0b86d2a51341b6b9744c3b7cc0c1b7f4f0559fc4
Mark <kastelic.nw@gmail.com>
2019-01-31 07:18:35 -0800

    Copied MK-3478-WeatherView views.py into MK-3475-TopMovies views.py

--- DataScrape/DataApp/views.py
+++ DataScrape/DataApp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.
+from .models import UserProfile
+from .viewmodels import WeatherScraper
+
+def weather_data(request):
+    #  Until user login is functional, user zip_code is simply retrieved from the
+    #  first record of the database.
+    user = get_object_or_404(UserProfile, id=1)
+    zipcode = user.zip_code
+
+    weather = WeatherScraper(zipcode)
+
+    # parse HTML to extract temp, humidity and update time to localWeather
+    return render(request, 'DataApp/weather_data.html', {'weather': weather})
