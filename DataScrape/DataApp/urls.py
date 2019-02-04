af2694a3c1e0e2ac28db5a1b3d3bf123fabc6af7
Sean Goldsmith <sean.goldsmith90@gmail.com>
2019-02-04 14:03:06 -0800

    Resolving final merge on conflict.

--- DataScrape/DataApp/urls.py
+++ DataScrape/DataApp/urls.py
@@ -5,9 +5,6 @@ from . import views
 urlpatterns = [
     path('weather', views.weather_data, name='weather'),
     path('movies', views.movie_data, name='movies'),
-<<<<<<< HEAD
     path('events', views.events_data, name='events')
-=======
     path('nasa', views.nasa_data, name='nasa')
->>>>>>> 3a372839e539cbfb3de605482f599a229124482e
 ]
