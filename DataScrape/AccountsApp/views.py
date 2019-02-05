5177243b092d84fe4fc8b6ea5eec18f827f455b9
Mark <kastelic.nw@gmail.com>
2019-02-05 09:40:43 -0800

    Files and changes to allow user to select and save favorite NHL team.

--- DataScrape/AccountsApp/views.py
+++ DataScrape/AccountsApp/views.py
@@ -16,15 +16,23 @@ def register(request): # each view function takes an HttpRequest object as a par
         email = request.POST['email']
         password = request.POST['password']
         zipcode = request.POST['zipcode']
+        favorite_nhl_team = request.POST['favorite_nhl_team']
 
         # use the User model's custom create_user helper method to insert values into the database under their correct fields; field = input name
         u1 = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, email=email, password=password) # this also returns a user object which...
-        u2 = UserProfile(user=u1, zip_code=zipcode) # we can pass into the UserProfile along with their zipcode.
+        u2 = UserProfile(user=u1, zip_code=zipcode, favorite_nhl_team=favorite_nhl_team) # we can pass into the UserProfile along with their zipcode.
         u1.save() # then we can save the changes to the database.
         u2.save()
         
         return redirect('login') # this currently redirects the user back to the homepage, potentially this can redirect a new user to a dashboard, etc.
 
     else:
+        #  Create a team_name_list by accessing values in the queryset of all objects in the 
+        #  HockeyTeam model.  Supply the team_name_list as context to the register template in
+        #  order to display a drop down list for the user to select a favorite team.
         nhl_team_list =  HockeyTeam.objects.all()
-        return render(request, 'accounts/register.html')
+        nhl_team_names = list(nhl_team_list.values('team_name'))
+        team_name_list = []
+        for team in nhl_team_names:
+            team_name_list.append(team['team_name'])
+        return render(request, 'accounts/register.html', {'team_name_list': team_name_list})
--- DataScrape/DataApp/models.py
+++ DataScrape/DataApp/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     zip_code = models.IntegerField()
-    favorite_hockey_team = models.CharField(max_length=50, default='')
+    favorite_nhl_team = models.CharField(max_length=50, default='')
 
 class HockeyTeam(models.Model):
     team_id = models.CharField(max_length=50)
